a1cde49628569fcded81889f8cb431f8fdd8f2d0
Tina <tparkmedia@gmail.com>
2018-06-21 12:28:26 -0700

    boom boom banana

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const
   app = require('express'),
   PORT = 3000
 
+app.get('/', (req, res) => {
+  res.json({ message: "BOOM BANANA BOOM" })
+})
+
 app.listen(PORT, () =>{
   console.log("It's Alive.")
 })
